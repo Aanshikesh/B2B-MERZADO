270d43617e814a54c5b6a3481e9b29cd12464781
Aanshikesh <sbrawat790@gmail.com>
2026-09-12 16:58:12 +0530

    fix: restore matched path in api/index.js for Vercel serverless Express

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 const app = require('../server/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  // If Vercel rewrote the URL to /api/index.js, restore the original incoming path
+  const matchedPath = req.headers['x-matched-path'];
+  const vercelPath = req.query?._vercel_path;
+
+  if (matchedPath && matchedPath.startsWith('/api')) {
+    req.url = matchedPath;
+  } else if (vercelPath) {
+    req.url = '/api/' + vercelPath;
+  }
+
+  return app(req, res);
+};
